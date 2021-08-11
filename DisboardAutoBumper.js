17fde7f31dc4d43dbe13526af718a5202b8a63e5
Theo <79630117+TheoTheEpic@users.noreply.github.com>
2021-08-11 14:24:00 +0100

    Main Script

---
+++ DisboardAutoBumper.js
@@ -0,0 +1,19 @@
+const config = require('./config.json')
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+async function bump(channel_id) {
+    const channel = client.channels.cache.get(channel_id) 
+    await channel.send('!d bump')
+    return true
+}
+
+client.on('ready', () => {
+      console.log('Auto-Bumper ready')
+      bump(config.channel_id)
+      setInterval(() => {
+            bump(config.channel_id)
+      }, 7200000)
+})
+
+client.login(token)
